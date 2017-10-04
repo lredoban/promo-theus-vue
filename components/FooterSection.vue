1747abf26d61570a82339c2687c481e8c217dd0c
gashmal <tovo@caocao-PC.localdomain>
2017-10-04 16:17:41 +0200

    fix link instagram footer

--- components/FooterSection.vue
+++ components/FooterSection.vue
@@ -9,7 +9,7 @@
         <ul>
             <li><a href="http://www.facebook.com/promo.theus.developpement" class="facebook" target="_blank"><SvgIcon name="facebook"/></a></li>
             <li><a href="//plus.google.com/101357276651616256581/about" class="googleplus" target="_blank"><SvgIcon name="googleplus"/></a></li>
-            <li><a href="http://www.instagram.com/promotheusdeveloppement/" class="instagram" target="_blank"><SvgIcon name="instagram"/></a></li>
+            <li><a href="https://www.instagram.com/promo.theus.developpement/" class="instagram" target="_blank"><SvgIcon name="instagram"/></a></li>
         </ul>
       </div>
   </section>
